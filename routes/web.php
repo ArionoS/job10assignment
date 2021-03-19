f4fb258dcdbe988931a590eaa4657614912de044
ArionoS <arionoseptian0802@gmail.com>
2021-03-19 09:29:04 +0700

    new1

--- routes/web.php
+++ routes/web.php
@@ -39,3 +39,11 @@ Route::get('/news', function () {
 });
 */
 
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
